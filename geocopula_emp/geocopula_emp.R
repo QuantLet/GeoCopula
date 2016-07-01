9507e123a103351baf82fc326ae628c375e029cb
chenshic.hub <chenshiq@hu-berlin.de>
2016-07-01 11:42:45 +0200

    update geocopula_emp

--- geocopula_emp/geocopula_emp.R
+++ geocopula_emp/geocopula_emp.R
@@ -13,31 +13,28 @@ lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 # load dataset of five countries
 load("fivecountries.RData")
 resultsres = commonans0915mac$vt  #uk, germany, france, italy, sweden
-
-#source("helpfun.r")
 transformC = function(data, method = "uniform") {
-  f1 = function(x) {
-  fn = ecdf(x)
-  y  = fn(x)
+  f1       = function(x) {
+  fn       = ecdf(x)
+  y        = fn(x)
   return(y)
   }
-  nr    = nrow(data)
-  nc    = ncol(data)
-  data1 = sapply(1:nc, function(i) f1(data[, i]))
+  nr       = nrow(data)
+  nc       = ncol(data)
+  data1    = sapply(1:nc, function(i) f1(data[, i]))
   colnames(data1) = colnames(data)
   rownames(data1) = rownames(data)
   data1
 }
 
 # the basic setting for optimization
-nu     = 0.5
-reltol = 0.01
-trace  = TRUE
-maxit  = 50000
-sigma  = 1
-cutoff = 2000 
-tlag   = 20 
-
+nu       = 0.5
+reltol   = 0.01
+trace    = TRUE
+maxit    = 50000
+sigma    = 1
+cutoff   = 2000 
+tlag     = 20 
 namelist = c("UK", "Germany", "France", "Italy", "Sweden")
 location = read.csv("location_country.csv")
 rownames(location) = location[, 1]
@@ -59,17 +56,14 @@ da       = as.data.frame(as.vector(t(data)))
 colnames(da) = "inf"
 inflation    = STFDF(location, time, da)
 summary(sort(spDists(inflation@sp)))
-
-#par(mar = c(1, 1, 1, 1))
-#plot(sort(spDists(inflation@sp)))
-vvd4 = variogram(inf ~ 1, inflation, width = 400, cutoff = 2000, tlags = 0:35)
+vvd4         = variogram(inf ~ 1, inflation, width = 400, cutoff = 2000, tlags = 0:35)
 vvd4[1, 2:3] = 0
 
-na_indi = which(is.na(vvd4), arr.ind = TRUE)
-vvd4t   = vvd4
-save(vvd4, file="vvd4.RData")
+na_indi  = which(is.na(vvd4), arr.ind = TRUE)
+vvd4t    = vvd4
+# save(vvd4, file="vvd4.RData")
 
 # produce plot
 plot(vvd4t, wireframe = T, xlab = list("distance (km)", rot = 30), 
-     ylab = list("time lag (days)", rot = -35), zlab = "gamma", 
+     ylab   = list("time lag (days)", rot = -35), zlab = "gamma", 
      scales = list(arrows = F, z = list(distance = 5)), zlim = c(0, 1.2))
